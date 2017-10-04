6ac0f3d2f4c92fa7b6b97660f28f6a78636b3c29
akira <a.kawashiro@gmail.com>
2017-10-05 00:17:28 +0900

    Make initial database.

--- register_webpage.py
+++ register_webpage.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import subprocess
 import nltk
 from nltk.corpus import stopwords
---
+++ setup_database.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import sqlite3
+
+dbname = 'keach.db'
+
+conn = sqlite3.connect(dbname)
+c = conn.cursor()
+
+create_table = "CREATE TABLE tfidf (word text,link text,tfidf real)"
+c.execute(create_table)
+create_table = "CREATE TABLE crawler (link text,last_date integer)"
+c.execute(create_table)
+create_table = "CREATE TABLE average_document (word text,number_of_document integer)"
+c.execute(create_table)
+create_table = "CREATE TABLE size_of_average_document (size integer)"
+c.execute(create_table)
+
