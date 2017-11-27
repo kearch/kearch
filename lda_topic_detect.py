35944ea8bf91419c2dedcdd758b8e1980c12772f
akira <a.kawashiro@gmail.com>
2017-11-27 23:58:31 +0900

    Made classifier.

--- lda_topic_detect.py
+++ lda_topic_detect.py
@@ -7,10 +7,33 @@ import pickle
 from sklearn.linear_model import SGDClassifier
 
 n_topic = 100
+IN_TOPIC = 0
+OUT_OF_TOPIC = 1
+
+
+class TopicClassifier(object):
+    def __init__(self):
+        self.dictionary = corpora.Dictionary.load_from_text('gensim.dict')
+        with open('lda.pickle', 'rb') as f:
+            self.lda = pickle.load(f)
+        with open('clf.pickle', 'rb') as f:
+            self.clf = pickle.load(f)
+
+    def classfy(self, words):
+        test_text = [words]
+        test_corpus = [self.dictionary.doc2bow(text) for text in test_text]
+        res = list()
+        for topics_per_document in self.lda[test_corpus]:
+            r = self.clf.predict([trans_vector(topics_per_document)])
+            res.append(r)
+        if res[0][0] == 0:
+            return IN_TOPIC
+        else:
+            return OUT_OF_TOPIC
 
 
 def sum_vector(vs):
-    a = [0 for i in range(0, 10)]
+    a = [0 for i in range(0, n_topic)]
     for v in vs:
         for (i, j) in v:
             a[i] += j / len(vs)
@@ -18,7 +41,7 @@ def sum_vector(vs):
 
 
 def trans_vector(v):
-    a = [0 for i in range(0, 10)]
+    a = [0 for i in range(0, n_topic)]
     for (i, j) in v:
         a[i] += j
     return a
@@ -64,38 +87,41 @@ if __name__ == '__main__':
             corpus=corpus, num_topics=n_topic, id2word=dictionary)
         with open('lda.pickle', 'wb') as f:
             pickle.dump(lda, f)
+
+        sc_samples = list()
+        sc_labels = list()
+
+        p = mult.Pool(mult.cpu_count())
+        texts = p.map(url_to_words, topic_urls[n_gensim_urls:])
+        p.close()
+        texts = list(filter(lambda x: not x == [], texts))
+        corpus = [dictionary.doc2bow(text) for text in texts]
+        for topics_per_document in lda[corpus]:
+            sc_samples.append(trans_vector(topics_per_document))
+            sc_labels.append(0)
+
+        p = mult.Pool(mult.cpu_count())
+        texts = p.map(url_to_words, random_urls[n_gensim_urls:])
+        p.close()
+        texts = list(filter(lambda x: not x == [], texts))
+        corpus = [dictionary.doc2bow(text) for text in texts]
+        for topics_per_document in lda[corpus]:
+            sc_samples.append(trans_vector(topics_per_document))
+            sc_labels.append(1)
+
+        clf = SGDClassifier(loss="hinge", penalty="l2")
+        print(sc_samples)
+        print(sc_labels)
+        clf.fit(sc_samples, sc_labels)
+        with open('clf.pickle', 'wb') as f:
+            pickle.dump(clf, f)
+
     else:
         dictionary = corpora.Dictionary.load_from_text('gensim.dict')
         with open('lda.pickle', 'rb') as f:
             lda = pickle.load(f)
-
-    print(lda.show_topics())
-
-    sc_samples = list()
-    sc_labels = list()
-
-    p = mult.Pool(mult.cpu_count())
-    texts = p.map(url_to_words, topic_urls[n_gensim_urls:])
-    p.close()
-    texts = list(filter(lambda x: not x == [], texts))
-    corpus = [dictionary.doc2bow(text) for text in texts]
-    for topics_per_document in lda[corpus]:
-        sc_samples.append(trans_vector(topics_per_document))
-        sc_labels.append(0)
-
-    p = mult.Pool(mult.cpu_count())
-    texts = p.map(url_to_words, random_urls[n_gensim_urls:])
-    p.close()
-    texts = list(filter(lambda x: not x == [], texts))
-    corpus = [dictionary.doc2bow(text) for text in texts]
-    for topics_per_document in lda[corpus]:
-        sc_samples.append(trans_vector(topics_per_document))
-        sc_labels.append(1)
-
-    clf = SGDClassifier(loss="hinge", penalty="l2")
-    print(sc_samples)
-    print(sc_labels)
-    clf.fit(sc_samples, sc_labels)
+        with open('clf.pickle', 'rb') as f:
+            clf = pickle.load(f)
 
     print("topic_urls")
     for u in topic_urls[:10]:
