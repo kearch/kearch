94cdac08143d78407f78bff1b12d7c909c316a0d
Akira KAWATA <a.kawashiro@gmail.com>
2017-12-01 10:09:27 +0900

    Add status_check.sh

---
+++ status_check.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+echo "Registered webpages:"
+sqlite3 keach.db "SELECT COUNT(*) FROM date_to_link where 10 < last_date"
+echo "Webpages in croll queue:"
+sqlite3 keach.db "SELECT COUNT(*) FROM date_to_link"
+echo "Unique url in pagerank_next:"
+sqlite3 keach.db "SELECT COUNT(DISTINCT link) FROM pagerank_next"
+echo "All url in pagerank_next:"
+sqlite3 keach.db "SELECT COUNT(*) FROM pagerank_next"
