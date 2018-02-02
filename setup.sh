c82f1b479cf22a53ad339bc6f595b7ae1400deac
Akira KAWATA <a.kawashiro@gmail.com>
2018-02-03 00:31:28 +0900

    change the name of detect file.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
-rm keach.db
+d=keach`date +"%Y%m%d%k%M%S"`.db
+mv keach.db ${d}
+
 python3 ./setup_database.py
 python3 ./average_document.py ./random_url_list
-python3 lda_topic_detect.py computer_science_url_list random_url_list
+python3 topic_detect.py computer_science_url_list random_url_list
