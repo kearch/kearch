65771d85bcba715cbb3634d10725fe454bc4a2fb
Akira KAWATA <a.kawashiro@gmail.com>
2018-03-14 20:30:54 +0900

    Switch off debug.

--- flask_main.py
+++ flask_main.py
@@ -3,19 +3,22 @@ from retrieve_query import retrieve
 
 app = Flask(__name__)
 
+
 @app.route('/search', methods=['GET', 'POST'])
 def post():
     if request.method == 'GET':
         query = request.args['query']
         results = retrieve(query)
-        return render_template('result.html',results=results,query=query)
+        return render_template('result.html', results=results, query=query)
     else:
         return redirect(url_for('index.html'))
+
 
 @app.route("/")
 def hello():
     return render_template('index.html')
 
+
 if __name__ == '__main__':
-    app.debug = True # デバッグモード有効化
-    app.run(host='0.0.0.0',port=2222) # どこからでもアクセス可能に
+    app.debug = False
+    app.run(host='0.0.0.0', port=2222)  # どこからでもアクセス可能に
