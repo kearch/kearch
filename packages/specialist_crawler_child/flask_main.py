b014b6459c46284691b9c18b1420c393bd55a2a4
Akira KAWATA <a.kawashiro@gmail.com>
2018-06-12 22:21:35 +0900

    Made api main page.

---
+++ packages/specialist_crawler_child/flask_main.py
@@ -0,0 +1,24 @@
+# Example for api is
+# http://localhost:10080/crawl_a_page?url=https%3A//shedopen.deviantart.com/
+# http://localhost:10080/crawl_a_page?url=https%3A//en.wikipedia.org/wiki/Haskell_%28programming_language%29
+
+import flask
+import crawler_child
+import urllib.parse
+
+app = flask.Flask(__name__)
+
+
+@app.route('/crawl_a_page', methods=['GET'])
+def post():
+    url_q = flask.request.args['url']
+    url = urllib.parse.unquote(url_q)
+    print(url, 'was given.')
+    result = crawler_child.url_to_json_string(url)
+    print('Got result.')
+    return flask.render_template('result.html', result=result)
+
+
+if __name__ == '__main__':
+    app.debug = False
+    app.run(host='0.0.0.0', port=10080)  # どこからでもアクセス可能に
