b835f2ebcd1ece6fc560ed031d11baa5471de393
gky360 <gky360@gmail.com>
2018-06-17 16:46:59 +0900

    update readme and test

--- packages/kearch_common/tests/test_requester.py
+++ packages/kearch_common/tests/test_requester.py
@@ -8,4 +8,16 @@ class TestRequester(unittest.TestCase):
     def test_get_request(self):
         requester = KearchRequester(
             'https://jsonplaceholder.typicode.com', requester_name='test_requester')
-        requester.request(method='GET', path='/posts')
+        resp = requester.request(
+            method='GET', path='/posts', params={'userId': 1})
+        print(resp.json())
+        assert(resp.status_code == 200)
+
+    def test_post_request(self):
+        requester = KearchRequester(
+            'https://jsonplaceholder.typicode.com', requester_name='test_requester')
+        payload = {'userId': 1, 'title': 'hello', 'body': 'world'}
+        resp = requester.request(
+            method='POST', path='/posts', payload=payload)
+        print(resp.json())
+        assert(resp.status_code == 201)
