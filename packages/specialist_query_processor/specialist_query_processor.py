71c7504840ad21e61210e8b7d0712f347dd0052e
Akira KAWATA <a.kawashiro@gmail.com>
2018-06-21 15:06:35 +0900

    Add unit test code.

--- packages/specialist_query_processor/specialist_query_processor.py
+++ packages/specialist_query_processor/specialist_query_processor.py
@@ -1,4 +1,5 @@
 from kearch_common.connection import KearchRequester
+from kearch_common.connection import get_payload
 
 DATABASE_IP = '192.168.11.11'
 DATABASE_PORT = 10080
@@ -22,11 +23,20 @@ def calculate_score(queries, result):
     return ret
 
 
+test_response = {'url': 'www.google.com', 'title_words': ['facebook', 'amazon'], 'summary': 'Hello world!', 'tfidf': {'google': 1.0, 'facebook': 2.0}}
+
+
 def retrieve(queries):
     database_requester = KearchRequester(DATABASE_IP, DATABASE_PORT, REQUESTER_NAME)
-    results = database_requester.request(path='/retrieve', method='GET', payload={'queries': queries})
+
+    response = database_requester.request(path='/retrieve', method='GET', payload={'queries': queries})
+    # Debug Code: Uncomment a following line when you want this module standalone.
+    # response = test_response
+
+    results = get_payload(response)
     ret = {'data': []}
     for r in results:
+        # If Flask cannnot convert data to correct format, I must convert them by hand.
         d = {'url': r['url'], 'title_words': r['title_words'], 'summary': r['summary'], 'score': calculate_score(queries, r)}
         ret['data'].append(d)
     return ret
