e6d242662feda911ccfca1bffdcfcf67d4466d4b
Akira KAWATA <a.kawashiro@gmail.com>
2018-06-27 21:59:43 +0900

    Add unit test.

--- packages/specialist_crawler_parent/main.py
+++ packages/specialist_crawler_parent/main.py
@@ -1,6 +1,7 @@
-from kearch_common.connection import KearchRequester
+from kearch_common.requester import KearchRequester
 from concurrent.futures import ThreadPoolExecutor
-from urllib.parse import quote
+import time
+import sys
 
 
 CRAWLER_CHILD_IP = '192.168.11.10'
@@ -13,32 +14,66 @@ NUM_THREAD = 5
 
 REQUESTER_NAME = 'specialist_crawler_parent'
 
+MAX_URLS = 100
+
+# When you change DEBUG_UNIT_TEST true, this program run unit test.
+DEBUG_UNIT_TEST = False
+
 
 def crawl_a_page(url):
-    crawler_requester = KearchRequester(CRAWLER_CHILD_IP, CRAWLER_CHILD_PORT, REQUESTER_NAME)
-    ret = crawler_requester.request(path='/crawl_a_page', params={'url': quote(url)})
+    if DEBUG_UNIT_TEST:
+        ret = {'url': 'www.google.com', 'title_words': ['google', 'USA'], 'summary': 'Google is the biggest IT company.',
+               'tfidf': {'google': 1.0}, 'inner_links': ['www.facebook.com'], 'outer_links': []}
+        time.sleep(2)
+        return ret
+    else:
+        crawler_requester = KearchRequester(CRAWLER_CHILD_IP, CRAWLER_CHILD_PORT, REQUESTER_NAME)
+        ret = crawler_requester.request(path='/crawl_a_page', params={'url': url})
+        return ret
+
+
+# DEBUG CODE: Following lines are for debug. They are codes for unit test.
+def get_next_urls_dummy(max_urls):
+    urls = []
+    for i in range(0, max_urls, 2):
+        urls.extend(['www.google.com', 'www.facebook.com'])
+    ret = {'urls': urls}
+    time.sleep(5)
     return ret
 
 
 if __name__ == '__main__':
     database_requester = KearchRequester(DATABASE_IP, DATABASE_PORT, REQUESTER_NAME)
 
-    urls_in_queue = database_requester.request(path='/get_next_urls', params={'max_urls': 100})
-    # TODO (kawata) some process to extract url list from urls_in_queue variable.
+    if DEBUG_UNIT_TEST:
+        resp = get_next_urls_dummy(MAX_URLS)
+    else:
+        resp = database_requester.request(path='/get_next_urls', params={'max_urls': MAX_URLS})
+    urls_in_queue = resp['urls']
     urls_to_push = list()
-    datum_to_push = list()
+    data_to_push = list()
 
     while True:
+        if DEBUG_UNIT_TEST:
+            print('length of queue = ', len(urls_in_queue), file=sys.stderr)
+
         if len(urls_in_queue) == 0:
-            # push datum to database
-            database_requester.request(path='/push_urls_to_queue', method='POST', payload={'urls': urls_to_push})
-            database_requester.request(path='/push_webpage_to_database', method='POST', payload={'datum': datum_to_push})
+            if DEBUG_UNIT_TEST:
+                # DEBUG CODE: Following lines are for debug. They are codes for unit test.
+                resp = get_next_urls_dummy(MAX_URLS)
+                urls_in_queue = resp['urls']
+            else:
+                # push data to database
+                database_requester.request(path='/push_urls_to_queue', method='POST', payload={'urls': urls_to_push})
+                database_requester.request(path='/push_webpage_to_database', method='POST', payload={'data': data_to_push})
 
-            # fetch urls from database
-            urls_in_queue = database_requester.request(path='/get_next_urls', params={'max_urls': 100})
+                # fetch urls from database
+                resp = database_requester.request(path='/get_next_urls', params={'max_urls': MAX_URLS})
+                urls_in_queue = resp['urls']
 
-        with ThreadPoolExecutor(max_workers=NUM_THREAD, thread_name_prefix="thread") as executor:
-            results = executor.map(crawl_a_page, urls_in_queue[:10])
+        with ThreadPoolExecutor(max_workers=NUM_THREAD) as executor:
+            results = executor.map(crawl_a_page, list(urls_in_queue[:NUM_THREAD]))
+        results = filter((lambda x: x == {}), results)
 
         for r in results:
             urls_to_push.extend(r.inner_links)
@@ -47,6 +82,6 @@ if __name__ == '__main__':
             d = r
             del d['inner_links']
             del d['outer_links']
-            datum_to_push.append(d)
+            data_to_push.append(d)
 
-        urls_in_queue = urls_in_queue[10:]
+        urls_in_queue = urls_in_queue[NUM_THREAD:]
