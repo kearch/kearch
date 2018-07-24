90d68c8784b9cedc324956d5b4e48551ed8fe45e
gky360 <gky360@gmail.com>
2018-07-24 14:11:06 +0900

    add title to webpages table

--- services/sp-db/sql/webpages_schema.sql
+++ services/sp-db/sql/webpages_schema.sql
@@ -1,5 +1,6 @@
 CREATE TABLE `webpages` (
   `url` varchar(255) NOT NULL,
+  `title` text DEFAULT NULL,
   `title_words` json DEFAULT NULL,
   `summary` text DEFAULT NULL,
   `tfidf` json DEFAULT NULL,
