c97a31eb3fb8aec89e9c97591de09325dff5aca3
Akira KAWATA <a.kawashiro@gmail.com>
2018-09-19 11:09:21 +0900

    Rename function.

--- packages/specialist_crawler_child/flask_main.py
+++ packages/specialist_crawler_child/flask_main.py
@@ -3,17 +3,15 @@
 # http://localhost:10080/crawl_a_page?url=https%3A//en.wikipedia.org/wiki/Haskell_%28programming_language%29
 
 import sys
-
 import flask
 from flask import jsonify
-
 import crawler_child
 
 app = flask.Flask(__name__)
 
 
 @app.route('/crawl_a_page', methods=['GET'])
-def post():
+def crawl_a_page():
     url = flask.request.args.get('url')
     print(url)
     sys.stderr.write(url + '\n')
