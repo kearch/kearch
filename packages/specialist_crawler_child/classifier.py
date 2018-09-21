61b01008078e729f021238e17e727b45e7aae31c
Akira KAWATA <a.kawashiro@gmail.com>
2018-09-21 14:12:36 +0900

    Made classifier.py.

--- packages/specialist_crawler_child/classifier.py
+++ packages/specialist_crawler_child/classifier.py
@@ -5,17 +5,20 @@ from gensim import corpora
 import webpage
 import argparse
 import multiprocessing as mult
-# import pickle
 import random
+import pickle
 from sklearn.naive_bayes import BernoulliNB
 import abc
+import tempfile
+import zipfile
+import os
 
 n_urls = 1000
 n_tests = 100
 IN_TOPIC = 0
 OUT_OF_TOPIC = 1
 
-CACHE_DIR = './classifier_cache/'
+PARAMS_FILE = 'classifier_cache_params.zip'
 
 
 class Classifier(metaclass=abc.ABCMeta):
@@ -23,6 +26,7 @@ class Classifier(metaclass=abc.ABCMeta):
     def learn_params(self, topic_url_list, random_url_list, language):
         pass
 
+    # This function dump all parameters to one json.
     @abc.abstractclassmethod
     def dump_params(self):
         pass
@@ -57,18 +61,18 @@ class NBTopicClassifier(Classifier):
     def learn_params_body(self, topic_urls, random_urls, language):
         n_gensim_urls = int(min(len(topic_urls), len(random_urls)) / 2)
 
-        sys.stderr.write('classifer.py -- Downloading start\n')
+        print('classifer.py -- Downloading start', file=sys.stderr)
         p = mult.Pool(mult.cpu_count() * 3)
         texts = p.map(self.url_to_words, topic_urls[
                       :n_gensim_urls] + random_urls[:n_gensim_urls])
-        sys.stderr.write('classifer.py -- Downloading finish\n')
+        print('classifer.py -- Downloading finish', file=sys.stderr)
 
         texts = list(filter(lambda x: not x == [], texts))
         p.close()
         self.dictionary_body = corpora.Dictionary(texts)
-        sys.stderr.write('classifer.py -- Dictionary size = ' + str(len(self.dictionary_body)) + '\n')
+        print('classifer.py -- Dictionary size = ' + str(len(self.dictionary_body)) + '', file=sys.stderr)
 
-        sys.stderr.write('classifer.py -- Making Classifier for main text start\n')
+        print('classifer.py -- Making Classifier for main text start', file=sys.stderr)
         sc_samples = list()
         sc_labels = list()
         p = mult.Pool(mult.cpu_count() * 3)
@@ -92,7 +96,7 @@ class NBTopicClassifier(Classifier):
 
         self.clf_body = BernoulliNB()
         self.clf_body.fit(sc_samples, sc_labels)
-        sys.stderr.write('classifer.py -- Making Classifier for main text finish\n')
+        print('classifer.py -- Making Classifier for main text finish', file=sys.stderr)
 
     def learn_params_title(self, topic_urls, random_urls, language):
         print('classifer.py -- Downloading start', file=sys.stderr)
@@ -149,17 +153,36 @@ class NBTopicClassifier(Classifier):
         else:
             return OUT_OF_TOPIC
 
-    def classfy_log_probability(self, text):
+    def classfy_log_probability_body(self, text):
         bow = self.dictionary_body.doc2bow(text)
         res = self.clf_body.predict_log_proba(
             [self.alist_to_vector(bow, self.dictionary_body)])
         return res[0]
 
-    def dump_params(self):
-        pass
+    def dump_params(self, filename):
+        with tempfile.TemporaryDirectory() as temp_path:
+            self.dictionary_body.save(os.path.join(temp_path, 'gensim_dictionary_body.pickle'))
+            self.dictionary_title.save(os.path.join(temp_path, 'gensim_dictionary_title.pickle'))
+            with open(os.path.join(temp_path, 'nb_clf_body.pickle'), 'wb') as f:
+                pickle.dump(self.clf_body, f)
+            with open(os.path.join(temp_path, 'nb_clf_title.pickle'), 'wb') as f:
+                pickle.dump(self.clf_title, f)
+            with zipfile.ZipFile(filename, 'w') as f:
+                f.write(os.path.join(temp_path, 'gensim_dictionary_body.pickle'), arcname='gensim_dictionary_body.pickle')
+                f.write(os.path.join(temp_path, 'gensim_dictionary_title.pickle'), arcname='gensim_dictionary_title.pickle')
+                f.write(os.path.join(temp_path, 'nb_clf_body.pickle'), arcname='nb_clf_body.pickle')
+                f.write(os.path.join(temp_path, 'nb_clf_title.pickle'), arcname='nb_clf_title.pickle')
 
-    def load_params(self):
-        pass
+    def load_params(self, filename):
+        with tempfile.TemporaryDirectory() as temp_path:
+            with zipfile.ZipFile(filename) as z:
+                z.extractall(temp_path)
+                self.dictionary_body = corpora.Dictionary.load(os.path.join(temp_path, 'gensim_dictionary_body.pickle'))
+                self.dictionary_title = corpora.Dictionary.load(os.path.join(temp_path, 'gensim_dictionary_title.pickle'))
+                with open(os.path.join(temp_path, 'nb_clf_body.pickle'), 'rb') as f:
+                    self.clf_body = pickle.load(f)
+                with open(os.path.join(temp_path, 'nb_clf_title.pickle'), 'rb') as f:
+                    self.clf_title = pickle.load(f)
 
 
 if __name__ == '__main__':
@@ -168,8 +191,7 @@ if __name__ == '__main__':
         "topic_url_list", help="sample webpages about topic")
     parser.add_argument(
         "random_url_list", help="random webpages")
-    parser.add_argument('--cache', help='use cache', action='store_true')
-    parser.add_argument('--ntopic', help='number of topics', nargs=1)
+    parser.add_argument('--show-test', help='test for independent dataset', action='store_true')
     args = parser.parse_args()
 
     with open(args.topic_url_list, 'r') as f:
@@ -189,26 +211,32 @@ if __name__ == '__main__':
     cls = NBTopicClassifier()
     cls.learn_params(topic_urls, random_urls, 'en')
 
-    true_positive = 0
-    true_negative = 0
-    false_positive = 0
-    false_negative = 0
-    for u in topic_urls1[n_urls:n_urls + n_tests]:
-        w = webpage.create_webpage_with_cache(u)
-        print(u, cls.classfy_log_probability(w.words))
-        if cls.classfy(w) == IN_TOPIC:
-            true_positive += 1
-        else:
-            true_negative += 1
-    for u in random_urls1[n_urls:n_urls + n_tests]:
-        w = webpage.create_webpage_with_cache(u)
-        if cls.classfy(w) == OUT_OF_TOPIC:
-            false_negative += 1
-        else:
-            false_positive += 1
-    precision = true_positive / (true_positive + false_positive + 1)
-    recall = true_positive / (true_positive + false_negative + 1)
-    fmeasure = 2 * precision * recall / (precision + recall + 1)
-    print('classifier.py -- TP=', true_positive, 'TN=', true_negative,
-          'FP=', false_positive, 'FN=', false_negative,
-          'precision=', precision, 'recall=', recall, 'fmeasure=', fmeasure)
+    cls.dump_params(PARAMS_FILE)
+
+    if args.show_test:
+        cls.load_params(PARAMS_FILE)
+        true_positive = 0
+        true_negative = 0
+        false_positive = 0
+        false_negative = 0
+        for u in topic_urls1[n_urls:n_urls + n_tests]:
+            w = webpage.create_webpage_with_cache(u)
+            print(u, cls.classfy_log_probability_body(w.words))
+            if cls.classfy(w) == IN_TOPIC:
+                true_positive += 1
+            else:
+                true_negative += 1
+        for u in random_urls1[n_urls:n_urls + n_tests]:
+            w = webpage.create_webpage_with_cache(u)
+            print(u, cls.classfy_log_probability_body(w.words))
+            if cls.classfy(w) == OUT_OF_TOPIC:
+                false_negative += 1
+            else:
+                false_positive += 1
+
+        precision = true_positive / (true_positive + false_positive + 1)
+        recall = true_positive / (true_positive + false_negative + 1)
+        fmeasure = 2 * precision * recall / (precision + recall + 1)
+        print('classifier.py -- TP=', true_positive, 'TN=', true_negative,
+              'FP=', false_positive, 'FN=', false_negative,
+              'precision=', precision, 'recall=', recall, 'fmeasure=', fmeasure)
