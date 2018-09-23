f80de66d082ddacec9f94af5c82123aee974e486
gky360 <gky360@gmail.com>
2018-09-24 02:46:13 +0900

    fix benchmarker

--- benchmark/benchmarker.py
+++ benchmark/benchmarker.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
+import argparse
 import random
 import re
-import urllib.request
+import urllib
 import os
 from bs4 import BeautifulSoup
 
@@ -10,12 +11,17 @@ URLS_FOR_EXTRACT = ["https://en.wikipedia.org/wiki/Programming_language",
                     "https://en.wikipedia.org/wiki/History",
                     "https://en.wikipedia.org/wiki/Kyoto"]
 
-URL_FOR_SEARCH = "http://163.43.108.218:2222/search?query="
 MAX_SIZE_OF_QUERY = 3
-MAX_CONCURRENT_USER = 1000
+CONCURRENCY_LIST = [20, 50, 100, 200, 500, 1000]
 SIZE_OF_TEST = 100
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--scheme', '-s', default='http')
+    parser.add_argument('--host', '-H', default='163.43.108.218')
+    parser.add_argument('--port', '-p', default=2222, type=int)
+    args = parser.parse_args()
+
     words = []
     for url in URLS_FOR_EXTRACT:
         f = urllib.request.urlopen(url)
@@ -28,13 +34,18 @@ if __name__ == '__main__':
             if r.match(str(s)) is not None:
                 words.append(r.match(str(s)).groups()[0])
 
-    for s in range(1, MAX_SIZE_OF_QUERY+1):
-        for u in list(range(20, 100, 20)) + list(range(200, MAX_CONCURRENT_USER, 200)):
-            with open('tmp_file_for_benchmark', 'w') as f:
+    for u in CONCURRENCY_LIST:
+        for s in range(1, MAX_SIZE_OF_QUERY+1):
+            with open('/tmp/tmp_file_for_benchmark', 'w') as f:
                 for t in range(0, SIZE_OF_TEST):
                     ws = random.sample(words, s)
-                    f.write(URL_FOR_SEARCH + '+'.join(ws) + '\n')
+                    query_str = urllib.parse.urlencode({'query': ' '.join(ws)})
+                    netloc = '{}:{}'.format(args.host, args.port)
+                    url_str = urllib.parse.urlunparse(
+                        (args.scheme, netloc, '/search', '', query_str, ''))
+                    f.write(url_str + '\n')
 
             print('======================================================')
             print('Size of query = ' + str(s) + ',Connections = ' + str(u))
-            os.system('siege --concurrent=' + str(u) + ' --time=10S --file=tmp_file_for_benchmark')
+            os.system('siege --concurrent=' + str(u) +
+                      ' --time=10S --file=/tmp/tmp_file_for_benchmark')
