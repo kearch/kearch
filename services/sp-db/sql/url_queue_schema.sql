b08a06f4a65c8c30991b49b4c01af6d5b9fca169
gky360 <gky360@gmail.com>
2018-10-02 22:33:04 +0900

    fix url_queue schema

--- services/sp-db/sql/url_queue_schema.sql
+++ services/sp-db/sql/url_queue_schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `url_queue` (
-  `id` int NOT NULL auto_increment,
+  `id` int NOT NULL AUTO_INCREMENT,
   `url` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY(`id`),
-  CONSTRAINT `url_unique` UNIQUE (`url`)
+  UNIQUE KEY `index_url_queue_on_url` (`url`)
 ) ENGINE=InnoDB CHARACTER SET=utf8mb4;
