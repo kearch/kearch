0cac12a23af05c9d3253a91dae5085c1ae282f49
Akira KAWATA <a.kawashiro@gmail.com>
2018-10-17 18:24:54 +0900

    Now we can use phrase search in Kearch.

--- packages/specialist_query_processor/specialist_query_processor.py
+++ packages/specialist_query_processor/specialist_query_processor.py
@@ -18,14 +18,17 @@ def retrieve(queries, max_urls):
 
     resp = elastic_requester.request(
         path='/' + ELASTIC_INDEX + '/' + ELASTIC_TYPE + '/_search?pretty',
-        payload={'query': {'match': {'text': queries[0]}}},
+        payload={'query': {'match_phrase': {'text': ' '.join(queries)}}},
         method='POST')
 
     print(resp['hits']['hits'], file=sys.stderr)
     results = {'data': []}
     for d in resp['hits']['hits']:
         results['data'].append(
-            [d['_source']['url'], d['_source']['title'], d['_source']['text'][0:200]])
+            {'url': d['_source']['url'],
+             'title': d['_source']['title'],
+             'summary': d['_source']['text'][0:200],
+             'score': d['_score']})
 
     # Debug : Uncomment a following line when you want this module standalone.
     # results = test_results
