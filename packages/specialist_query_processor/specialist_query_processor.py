5960565a2b235d1c6f15a31ab1e604c1318497a7
Akira KAWATA <a.kawashiro@gmail.com>
2018-10-17 17:42:23 +0900

    Change the backend of retrieve from mysql to elastic search.

--- packages/specialist_query_processor/specialist_query_processor.py
+++ packages/specialist_query_processor/specialist_query_processor.py
@@ -1,24 +1,33 @@
 from kearch_common.requester import KearchRequester
+import sys
 
-DATABASE_HOST = 'sp-db.kearch.svc.cluster.local'
-DATABASE_PORT = 3306
+ELASTIC_HOST = 'sp-es.kearch.svc.cluster.local'
+ELASTIC_PORT = 9200
+ELASTIC_INDEX = 'sp'
+ELASTIC_TYPE = 'webpage'
 
 REQUESTER_NAME = 'specialist_query_processor'
-TFIDF_COEFFICIENT = 1.0
 
-
-test_results = {'data': [{'url': 'www.google.com', 'title_words': ['facebook', 'amazon'],
-                          'summary': 'Hello world!', 'tfidf': {'google': 1.0, 'facebook': 2.0}}]}
+test_results = {
+    'data': [['www.google.com', 'google home', 'google is strong']]}
 
 
 def retrieve(queries, max_urls):
-    database_requester = KearchRequester(
-        DATABASE_HOST, DATABASE_PORT, REQUESTER_NAME, conn_type='sql')
+    elastic_requester = KearchRequester(
+        ELASTIC_HOST, ELASTIC_PORT, REQUESTER_NAME, conn_type='elastic')
 
-    results = database_requester.request(
-        path='/retrieve_webpages',
-        params={'queries': queries, 'max_urls': max_urls})
-    # Debug Code: Uncomment a following line when you want this module standalone.
+    resp = elastic_requester.request(
+        path='/' + ELASTIC_INDEX + '/' + ELASTIC_TYPE + '/_search?pretty',
+        payload={'query': {'match': {'text': queries[0]}}},
+        method='POST')
+
+    print(resp['hits']['hits'], file=sys.stderr)
+    results = {'data': []}
+    for d in resp['hits']['hits']:
+        results['data'].append(
+            [d['_source']['url'], d['_source']['title'], d['_source']['text'][0:200]])
+
+    # Debug : Uncomment a following line when you want this module standalone.
     # results = test_results
 
     return results
