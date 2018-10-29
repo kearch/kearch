6ab7e0aa7eaaab6199353a887733b51b710baad9
gky360 <gky360@gmail.com>
2018-10-29 11:24:31 +0900

    add tables for requests between me and sp

---
+++ services/sp-db/sql/me_hosts_schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `me_hosts` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY(`id`),
+  UNIQUE KEY `index_me_hosts_on_name` (`name`)
+) ENGINE=InnoDB CHARACTER SET=utf8mb4;
