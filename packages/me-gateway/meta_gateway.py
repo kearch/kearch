216992b404aeb06eb0ada660f5f32bade8afb66f
Akira KAWATA <a.kawashiro@gmail.com>
2018-11-04 14:09:19 +0900

    Rename constant about config.

--- packages/me-gateway/meta_gateway.py
+++ packages/me-gateway/meta_gateway.py
@@ -6,8 +6,8 @@ DATABASE_HOST = 'me-db.kearch.svc.cluster.local'
 DATABASE_PORT = 10080
 REQUESTER_NAME = 'meta_gateway'
 
-CONNECTION_POLICY = 'connection_policy'
-HOST_NAME = 'host_name'
+CONFIG_CONNECTION_POLICY = 'connection_policy'
+CONFIG_HOST_NAME = 'host_name'
 
 
 def retrieve(sp_host, queries, max_urls):
@@ -35,10 +35,10 @@ def add_a_connection_request(sp_host):
     db = KearchRequester(DATABASE_HOST, DATABASE_PORT, REQUESTER_NAME,
                          conn_type='sql')
     config = db.request(path='/me/db/get_config_variables', method='GET')
-    if config[CONNECTION_POLICY] == 'public':
+    if config[CONFIG_CONNECTION_POLICY] == 'public':
         sp = KearchRequester(sp_host, SPECIALIST_GATEWAY_PORT, REQUESTER_NAME)
         dump = sp.request(path='/sp/gateway/get_a_dump',
-                          params={'me_host': config[HOST_NAME]})
+                          params={'me_host': config[CONFIG_HOST_NAME]})
         res = db.request(path='/add_new_sp_server',
                          payload={'host': sp_host, 'summary': dump})
         return res
@@ -52,7 +52,7 @@ def send_a_connection_request(sp_host):
     db = KearchRequester(DATABASE_HOST, DATABASE_PORT, REQUESTER_NAME,
                          conn_type='sql')
     config = db.request(path='/me/db/get_config_variables', method='GET')
-    me_host = config[HOST_NAME]
+    me_host = config[CONFIG_HOST_NAME]
 
     kr = KearchRequester(sp_host, SPECIALIST_GATEWAY_PORT, REQUESTER_NAME)
     res = kr.request(path='/sp/gateway/add_a_connection_request',
