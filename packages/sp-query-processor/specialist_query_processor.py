5265760a1e14c6eb8dc3648b1960347be7a8f975
Akira KAWATA <a.kawashiro@gmail.com>
2018-11-07 23:35:47 +0900

    Use title.

--- packages/sp-query-processor/specialist_query_processor.py
+++ packages/sp-query-processor/specialist_query_processor.py
@@ -8,18 +8,18 @@ ELASTIC_TYPE = 'webpage'
 
 REQUESTER_NAME = 'specialist_query_processor'
 
-test_results = {
-    'data': [['www.google.com', 'google home', 'google is strong']]}
-
 
 def retrieve(queries, max_urls):
     elastic_requester = KearchRequester(
         ELASTIC_HOST, ELASTIC_PORT, REQUESTER_NAME, conn_type='elastic')
 
+    query = ' '.join(queries)
+    payload = {'query': {'multi_match': {'query': query,
+                                         'type': 'phrase',
+                                         'fields': ['title', 'text']}}}
     resp = elastic_requester.request(
         path='/' + ELASTIC_INDEX + '/' + ELASTIC_TYPE + '/_search?pretty',
-        payload={'query': {'match_phrase': {'text': ' '.join(queries)}}},
-        method='POST')
+        payload=payload, method='POST')
 
     hits = []
     if 'hits' in resp and 'hits' in resp['hits']:
@@ -33,8 +33,5 @@ def retrieve(queries, max_urls):
              'title': d['_source']['title'],
              'summary': d['_source']['text'][0:200],
              'score': d['_score']})
-
-    # Debug : Uncomment a following line when you want this module standalone.
-    # results = test_results
 
     return results
