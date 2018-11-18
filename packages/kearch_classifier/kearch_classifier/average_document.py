d9f7746bc97aad069fa2f5da27e5396ac7b47ab6
Akira KAWATA <a.kawashiro@gmail.com>
2018-11-18 22:48:40 +0900

    Purify.

--- packages/kearch_classifier/kearch_classifier/average_document.py
+++ packages/kearch_classifier/kearch_classifier/average_document.py
@@ -6,7 +6,7 @@ import pickle
 import sys
 import traceback
 
-import kearch_classifier.webpage
+from kearch_classifier.webpage import create_webpage_with_cache, WebpageError
 
 CACHE_FILE = 'average_document.pickle'
 
@@ -21,9 +21,9 @@ def get_words(link_and_language):
     language = link_and_language[1]
     ws = list()
     try:
-        web = kearch_classifier.webpage.create_webpage_with_cache(link, language)
+        web = create_webpage_with_cache(link, language)
         ws = list(set(web.words))
-    except kearch_classifier.webpage.WebpageError:
+    except WebpageError:
         traceback.print_exc()
     return ws
 
