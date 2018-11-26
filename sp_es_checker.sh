c37c09c6c89df703720979ca7e2656dea20bc87f
Akira KAWATA <a.kawashiro@gmail.com>
2018-11-26 22:00:03 +0900

    Add newline at the end of sp_es_checker.sh.

--- sp_es_checker.sh
+++ sp_es_checker.sh
@@ -2,3 +2,4 @@
 
 sp_admin_pod_name=$(kubectl --namespace=kearch get po -l engine=sp,app=admin -o go-template --template '{{(index .items 0).metadata.name}}')
 kubectl exec -it --namespace=kearch ${sp_admin_pod_name} curl sp-es.kearch.svc.cluster.local:9200/sp/_count
+echo 
