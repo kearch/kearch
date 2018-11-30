19886a589bef41c825ce8b92506fe2f4509c8d7c
Akira KAWATA <a.kawashiro@gmail.com>
2018-11-30 18:14:03 +0900

    Use me-evaluater in me-query-processor.

--- packages/me-query-processor/meta_query_processor.py
+++ packages/me-query-processor/meta_query_processor.py
@@ -1,12 +1,13 @@
-import sys
 from kearch_common.requester import KearchRequester
 
 DATABASE_HOST = 'me-db.kearch.svc.cluster.local'
 DATABASE_PORT = 3306
-REQUESTER_NAME = 'meta_query_processor'
-
+EVALUATER_HOST = 'me-evaluater.kearch.svc.cluster.local'
+EVALUATER_PORT = 10080
 GATEWAY_HOST = 'me-gateway.kearch.svc.cluster.local'
 GATEWAY_PORT = 10080
+REQUESTER_NAME = 'meta_query_processor'
+SP_HOST_RATIO = [8, 1, 1]
 
 
 class MeQueryProcessorException(Exception):
@@ -14,47 +15,30 @@ class MeQueryProcessorException(Exception):
         Exception.__init__(self, e)
 
 
-def get_sp_host_from_database(queries):
-    database_requester = KearchRequester(
-        DATABASE_HOST, DATABASE_PORT, REQUESTER_NAME, conn_type="sql")
-    ret = database_requester.request(
-        path='/retrieve_sp_servers', params={'queries': queries})
-    return ret
-
-
-def get_result_from_sp(sp_host, queries, max_urls):
-    ret = dict()
-    sp_requester = KearchRequester(
-        GATEWAY_HOST, GATEWAY_PORT, REQUESTER_NAME)
-    ret = sp_requester.request(
+def get_result_from_sp(sp_host, query, max_urls):
+    g_req = KearchRequester(GATEWAY_HOST, GATEWAY_PORT, REQUESTER_NAME)
+    r = g_req.request(
         path='/retrieve',
-        params={'sp_host': sp_host, 'queries': ' '.join(queries),
-                'max_urls': max_urls})
-    ret['sp_host'] = sp_host
-    return ret
-
-
-def retrieve(queries, max_urls, sp=None):
-    if sp is None:
-        sp_data = get_sp_host_from_database(queries)
-        host_to_score = dict()
-        for d in sp_data.values():
-            for host, freq in d.items():
-                if host in host_to_score:
-                    host_to_score[host] += freq
-                else:
-                    host_to_score[host] = freq
-        if len(host_to_score) == 0:
-            sys.stderr.write("No specialist server in this meta database.\n")
-            return {
-                'data': [],
-                'sp_host': ''
-            }
-
-        host_to_score_list = list(host_to_score.items())
-        host_to_score_list.sort(key=lambda x: x[1], reverse=True)
-        best_host = host_to_score_list[0][0]
-    else:
-        best_host = sp
-    res = get_result_from_sp(best_host, queries, max_urls)
+        params={'sp_host': sp_host, 'queries': query, 'max_urls': max_urls})
+    res = list()
+    for d in r['data']:
+        d['sp_host'] = sp_host
+        res.append(d)
     return res
+
+
+def retrieve(query, max_urls, sp=None):
+    if sp is None:
+        e_req = KearchRequester(EVALUATER_HOST, EVALUATER_PORT, REQUESTER_NAME)
+        sp_hosts = e_req.request(path='/me/evaluater/evaluate',
+                                 params={'query': query})
+        a = list(sp_hosts.items()).sort(key=lambda x: x[1], reverse=True)
+        n = min(len(SP_HOST_RATIO), len(a))
+        res = list()
+        for i in range(0, n):
+            m = max_urls*SP_HOST_RATIO[i]/sum(SP_HOST_RATIO[:n])
+            res.append(get_result_from_sp(a[i][0], query, m))
+        return {'data': res}
+    else:
+        res = get_result_from_sp(sp, query, max_urls)
+        return {'data': res}
