6b1ed900528e2879095541bbeab523843d2149c8
Akira KAWATA <a.kawashiro@gmail.com>
2019-01-08 23:00:28 +0900

    Add scheme field to *_gateway_spec.yaml .

--- sp_gateway_server/python/openapi_server/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-
-# flake8: noqa
-from __future__ import absolute_import
-# import models into model package
-from openapi_server.models.connection_request_on_sp import ConnectionRequestOnSP
-from openapi_server.models.document import Document
-from openapi_server.models.inline_response200 import InlineResponse200
-from openapi_server.models.summary import Summary
