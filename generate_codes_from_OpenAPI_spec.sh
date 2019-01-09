83b5b5619aa1f0e9b80af09342c85a91dc4e5811
Akira KAWATA <a.kawashiro@gmail.com>
2019-01-10 01:46:58 +0900

    Use local spec file.

--- generate_codes_from_OpenAPI_spec.sh
+++ generate_codes_from_OpenAPI_spec.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i https://raw.githubusercontent.com/kearch/kearch/dev/me_gateway_spec.yaml -g python -o /local/me-gateway-client
-docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i https://raw.githubusercontent.com/kearch/kearch/dev/me_gateway_spec.yaml -g python-flask -o /local/me-gateway-server
-docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i https://raw.githubusercontent.com/kearch/kearch/dev/sp_gateway_spec.yaml -g python -o /local/sp-gateway-client
-docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i https://raw.githubusercontent.com/kearch/kearch/dev/sp_gateway_spec.yaml -g python-flask -o /local/sp-gateway-server
+docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/me_gateway_spec.yaml -g python -o /local/me-gateway-client
+docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/me_gateway_spec.yaml -g python-flask -o /local/me-gateway-server
+docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/sp_gateway_spec.yaml -g python -o /local/sp-gateway-client
+docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/sp_gateway_spec.yaml -g python-flask -o /local/sp-gateway-server
