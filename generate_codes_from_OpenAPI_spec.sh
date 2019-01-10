35b2a62103cde689577e4a0f37936b6735333b3a
Akira KAWATA <a.kawashiro@gmail.com>
2019-01-10 16:22:49 +0900

    Rename directory.

--- generate_codes_from_OpenAPI_spec.sh
+++ generate_codes_from_OpenAPI_spec.sh
@@ -2,9 +2,9 @@
 
 USERNAME=$(whoami)
 
-docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/me_gateway_spec.yaml -g python -o /local/me-gateway-client
+docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/me_gateway_spec.yaml -g python -o /local/me-gateway-client -c /local/openapi-me-gateway-config.json
 docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/me_gateway_spec.yaml -g python-flask -o /local/me-gateway-server
-docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/sp_gateway_spec.yaml -g python -o /local/sp-gateway-client
+docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/sp_gateway_spec.yaml -g python -o /local/sp-gateway-client -c /local/openapi-sp-gateway-config.json
 docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/sp_gateway_spec.yaml -g python-flask -o /local/sp-gateway-server
 
 sudo chown -R ${USERNAME}:${USERNAME} me-gateway-client
