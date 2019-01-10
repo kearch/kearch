8d899825b338b4668cfca754b4e58f68972a4596
Akira KAWATA <a.kawashiro@gmail.com>
2019-01-10 12:35:28 +0900

    Change the port.

--- packages/me-gateway-server/openapi_server/__main__.py
+++ packages/me-gateway-server/openapi_server/__main__.py
@@ -9,7 +9,7 @@ def main():
     app = connexion.App(__name__, specification_dir='./openapi/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('openapi.yaml', arguments={'title': 'Kearch meta search engine gateway API'})
-    app.run(port=8080)
+    app.run(port=10080)
 
 
 if __name__ == '__main__':
