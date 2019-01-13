752451d36beee6f6a09bad34a4ba6debd84fbbb9
gky360 <gky360@gmail.com>
2019-01-13 17:47:04 +0900

    fix paths of static files

--- packages/sp-admin/webpack.config.js
+++ packages/sp-admin/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   mode: 'development',
 
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'static', 'dist'),
     filename: '[name].bundle.js',
     publicPath: '/'
   },
