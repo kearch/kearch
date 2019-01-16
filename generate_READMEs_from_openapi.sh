93795fac3242266bd06089c030681fa7d8c79a74
Akira KAWATA <a.kawashiro@gmail.com>
2019-01-16 22:36:27 +0900

    Use widdershins.

--- generate_READMEs_from_openapi.sh
+++ generate_READMEs_from_openapi.sh
@@ -1,39 +1,11 @@
 #! /bin/bash
 
-docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/packages/sp-gateway/sp_gateway_spec.yaml -g html -o /local/
-sudo chown $USER:$USER index.html
-mv index.html packages/sp-gateway/README.md
-
-docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/packages/me-gateway/me_gateway_spec.yaml -g html -o /local/
-sudo chown $USER:$USER index.html
-mv index.html packages/me-gateway/README.md
-
-docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/packages/sp-classifier/sp_classifier_spec.yaml -g html -o /local/
-sudo chown $USER:$USER index.html
-mv index.html packages/sp-classifier/README.md
-
-docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/packages/sp-admin/sp_admin_spec.yaml -g html -o /local/
-sudo chown $USER:$USER index.html
-mv index.html packages/sp-admin/README.md
-
-docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/packages/sp-crawler-child/sp_crawler_child_spec.yaml -g html -o /local/
-sudo chown $USER:$USER index.html
-mv index.html packages/sp-crawler-child/README.md
-
-docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/packages/me-admin/me_admin_spec.yaml -g html -o /local/
-sudo chown $USER:$USER index.html
-mv index.html packages/me-admin/README.md
-
-docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/packages/sp-front/sp_front_spec.yaml -g html -o /local/
-sudo chown $USER:$USER index.html
-mv index.html packages/sp-front/README.md
-
-docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/packages/me-front/me_front_spec.yaml -g html -o /local/
-sudo chown $USER:$USER index.html
-mv index.html packages/me-front/README.md
-
-docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/packages/me-evaluater/me_evaluater_spec.yaml -g html -o /local/
-sudo chown $USER:$USER index.html
-mv index.html packages/me-evaluater/README.md
-
-sudo rm -rf ./.openapi-generator-ignore ./.openapi-generator
+widdershins -c packages/sp-gateway/sp_gateway_spec.yaml -o packages/sp-gateway/README.md
+widdershins -c packages/me-gateway/me_gateway_spec.yaml -o packages/me-gateway/README.md
+widdershins -c packages/sp-classifier/sp_classifier_spec.yaml -o packages/sp-classifier/README.md
+widdershins -c packages/sp-admin/sp_admin_spec.yaml -o packages/sp-admin/README.md
+widdershins -c packages/sp-crawler-child/sp_crawler_child_spec.yaml packages/sp-crawler-child/README.md
+widdershins -c packages/me-admin/me_admin_spec.yaml -o packages/me-admin/README.md
+widdershins -c packages/sp-front/sp_front_spec.yaml -o packages/sp-front/README.md
+widdershins -c packages/me-front/me_front_spec.yaml -o packages/me-front/README.md
+widdershins -c packages/me-evaluater/me_evaluater_spec.yaml -o packages/me-evaluater/README.md
