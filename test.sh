cd14a136fc838e7810700e4316d6eeb6db9dc0ce
Akira KAWATA <a.kawashiro@gmail.com>
2019-01-25 16:28:01 +0900

    Test for me-summary-updater.

--- test.sh
+++ test.sh
@@ -11,3 +11,7 @@ kubectl --namespace=kearch exec ${me_gateway_pod_name} pytest meta_gateway.py
 me_query_processor_pod_name=$(kubectl --namespace=kearch get po -l engine=me,app=query-processor -o go-template --template '{{(index .items 0).metadata.name}}')
 echo $me_query_processor_pod_name
 kubectl --namespace=kearch exec ${me_query_processor_pod_name} pytest meta_query_processor.py
+
+me_summary_updater_pod_name=$(kubectl --namespace=kearch get po -l engine=me,app=summary-updater -o go-template --template '{{(index .items 0).metadata.name}}')
+echo $me_summary_updater_pod_name
+kubectl --namespace=kearch exec ${me_summary_updater_pod_name} pytest main.py
