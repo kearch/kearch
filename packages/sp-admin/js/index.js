d09ec35453e4533df3ca0d28288e5240161716cd
gky360 <gky360@gmail.com>
2019-02-01 22:34:26 +0900

    sp-admin send delete request with jqyery ajax

--- packages/sp-admin/js/index.js
+++ packages/sp-admin/js/index.js
@@ -1,6 +1,22 @@
 import M from "materialize-css";
 import "../css/index.scss";
+import $ from 'jquery';
 
 document.addEventListener('DOMContentLoaded', function() {
   M.AutoInit();
 });
+
+window.deleteAConnectionRequest = (me_host) => {
+  console.log({ me_host });
+  if (confirm('Are you sure?')) {
+    $.ajax({
+      url: '/sp/admin/delete_a_connection_request',
+      type: 'DELETE',
+      data: { me_host },
+    }).done(() => {
+      window.location.replace('/');
+    }).fail(() => {
+      alert("Failed to delete a connection request.");
+    })
+  }
+}
