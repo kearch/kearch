618a6961ef8dbcc1534e66071f66aa6aab52d2ba
gky360 <gky360@gmail.com>
2019-02-01 23:15:51 +0900

    sp-admin remove jquery

--- packages/sp-admin/js/index.js
+++ packages/sp-admin/js/index.js
@@ -1,23 +1,6 @@
 import M from "materialize-css";
 import "../css/index.scss";
-import $ from 'jquery';
 
 document.addEventListener('DOMContentLoaded', function() {
   M.AutoInit();
 });
-
-window.deleteAConnectionRequest = (me_host) => {
-  console.log({ me_host });
-  console.log({ data: $(this).data() });
-  if (confirm('Are you sure?')) {
-    $.ajax({
-      url: '/sp/admin/delete_a_connection_request',
-      type: 'DELETE',
-      data: { me_host },
-    }).done(() => {
-      window.location.replace('/');
-    }).fail(() => {
-      alert("Failed to delete a connection request.");
-    })
-  }
-}
